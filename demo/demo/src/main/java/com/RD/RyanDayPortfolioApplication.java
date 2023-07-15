4a1931bd15a4b6efeefa78d1ba2e500a9f1fe9bb
Ryan Day <ryan0699@gmail.com>
2023-07-15 15:01:34 -0400

    Spring application loaded up

---
+++ demo/demo/src/main/java/com/RD/RyanDayPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.RD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanDayPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyanDayPortfolioApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/com/RD/RyanDayPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyanDayPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
